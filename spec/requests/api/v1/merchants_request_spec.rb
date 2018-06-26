7a79168ee5e72889d04f7355380e43c28e274707
Justine <jjtroyke@gmail.com>
2018-06-26 11:34:55 -0600

    crud for merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -22,4 +22,40 @@ describe 'Merchant API' do
     expect(response).to be_successful
     expect(merchant["id"]).to eq(id)
   end
+
+  it "can create a new merchant" do
+    merchant_params = { name: "Bubbles" }
+
+    post "/api/v1/merchants", params: {merchant: merchant_params}
+    merchant = Merchant.last
+
+    assert_response :success
+    expect(response).to be_successful
+    expect(merchant.name).to eq(merchant_params[:name])
+  end
+
+  it "can update an existing merchant" do
+    id = create(:merchant).id
+    previous_name = Merchant.last.name
+    merchant_params = { name: "Air" }
+
+    put "/api/v1/merchants/#{id}", params: {merchant: merchant_params}
+    merchant = Merchant.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(merchant.name).to_not eq(previous_name)
+    expect(merchant.name).to eq("Air")
+  end
+
+  it "can destroy an merchant" do
+    merchant = create(:merchant)
+
+    expect(Merchant.count).to eq(1)
+
+    delete "/api/v1/merchants/#{merchant.id}"
+
+    expect(response).to be_successful
+    expect(Merchant.count).to eq(0)
+    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
