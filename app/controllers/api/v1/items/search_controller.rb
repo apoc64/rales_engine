8d7db5847216329ccec0c7e6bcd459401969e270
apoc64 <sschwedt@gmail.com>
2018-06-27 16:49:10 -0600

    resolves issues with unit_prices

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -14,6 +14,11 @@ class Api::V1::Items::SearchController < ApplicationController
   private
 
   def items_params
-    params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
+    permitted = params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
+    if permitted[:unit_price]
+      permitted[:unit_price] = (((permitted[:unit_price].to_f) * 100) + 0.0001).round
+    end
+    permitted
+
   end
 end
