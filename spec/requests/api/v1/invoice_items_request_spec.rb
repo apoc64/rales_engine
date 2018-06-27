d35d877ef2af5dcf88b76225582a12ce18cd599c
apoc64 <sschwedt@gmail.com>
2018-06-27 16:32:58 -0600

    passes all invoice item endpoint tests

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -97,5 +97,75 @@ describe 'Invoice Items API' do
     expect(invoice_items.count).to eq(1)
   end
 
+  it 'can find all invoice_items by item_id' do
+    create_list(:invoice_item, 3)
+    original_ii = create(:invoice_item)
+    create(:invoice_item, item_id: original_ii.item_id)
 
+    get "/api/v1/invoice_items/find_all?item_id=#{original_ii.item_id}"
+
+    expect(response).to be_successful
+    invoice_items = JSON.parse(response.body)
+    expect(invoice_items.count).to eq(2)
+  end
+
+  it 'can find all invoice_items by invoice_id' do
+    create_list(:invoice_item, 3)
+    original_ii = create(:invoice_item)
+    create(:invoice_item, invoice_id: original_ii.invoice_id)
+
+    get "/api/v1/invoice_items/find_all?invoice_id=#{original_ii.invoice_id}"
+
+    expect(response).to be_successful
+    invoice_items = JSON.parse(response.body)
+    expect(invoice_items.count).to eq(2)
+  end
+
+  it 'can find all invoice_items by quantity' do
+    create_list(:invoice_item, 3)
+    original_ii = create(:invoice_item, quantity: 7)
+    create(:invoice_item, quantity: original_ii.quantity)
+
+    get "/api/v1/invoice_items/find_all?quantity=#{original_ii.quantity}"
+
+    expect(response).to be_successful
+    invoice_items = JSON.parse(response.body)
+    expect(invoice_items.count).to eq(2)
+  end
+
+  it 'can find all invoice_items by unit_price' do
+    create_list(:invoice_item, 3)
+    original_ii = create(:invoice_item, unit_price: 7000)
+    create(:invoice_item, unit_price: original_ii.unit_price)
+
+    get "/api/v1/invoice_items/find_all?unit_price=70"
+
+    expect(response).to be_successful
+    invoice_items = JSON.parse(response.body)
+    expect(invoice_items.count).to eq(2)
+  end
+
+    it 'can find all invoice_items by created_at' do
+      create_list(:invoice_item, 3)
+      original_ii = create(:invoice_item, created_at: '2012-03-27T14:57:57.000Z')
+      create(:invoice_item, created_at: original_ii.created_at)
+
+      get "/api/v1/invoice_items/find_all?created_at=2012-03-27T14:57:57.000Z"
+
+      expect(response).to be_successful
+      invoice_items = JSON.parse(response.body)
+      expect(invoice_items.count).to eq(2)
+    end
+
+    it 'can find all invoice_items by updated_at' do
+      create_list(:invoice_item, 3)
+      original_ii = create(:invoice_item, updated_at: '2012-03-27T14:57:57.000Z')
+      create(:invoice_item, updated_at: original_ii.updated_at)
+
+      get "/api/v1/invoice_items/find_all?updated_at=2012-03-27T14:57:57.000Z"
+
+      expect(response).to be_successful
+      invoice_items = JSON.parse(response.body)
+      expect(invoice_items.count).to eq(2)
+    end
 end
