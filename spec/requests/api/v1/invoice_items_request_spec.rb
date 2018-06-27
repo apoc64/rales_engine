5791a2698c5ab9e520c25e2e5c5fa7d0a519b265
apoc64 <sschwedt@gmail.com>
2018-06-27 15:20:20 -0600

    invoice items can find single ii by id

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -20,4 +20,24 @@ describe 'Invoice Items API' do
     invoice_item = JSON.parse(response.body)
     expect(invoice_item["id"]).to eq(id)
   end
+
+  it 'can find an invoice_item by id' do
+    id = create(:invoice_item).id
+
+    get "/api/v1/invoice_items/find?id=#{id}"
+
+    expect(response).to be_successful
+    invoice_item = JSON.parse(response.body)
+    expect(invoice_item["id"]).to eq(id)
+  end
+  #
+  # it 'can find an item by description' do
+  #   create_list(:item, 2)
+  #   original_item = create(:other_item)
+  #
+  #   get "/api/v1/items/find?description=#{original_item.description}"
+  #   expect(response).to be_successful
+  #   item = JSON.parse(response.body)
+  #   expect(item["id"]).to eq(original_item.id)
+  # end
 end
