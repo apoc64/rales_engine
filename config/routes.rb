a5371af6d05d0a4c044b1b77aa064747f9564af8
Justine <jjtroyke@gmail.com>
2018-06-27 16:31:58 -0600

    Invoice relationship endpoints complete

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,11 @@ Rails.application.routes.draw do
       namespace :invoices do
         get 'find', to: 'search#show'
         get '/find_all', to: 'search#index'
-        get '/:merchant_id/items', to: 'items#index'
+        get '/:id/items', to: 'items#index'
         get '/:id/transactions', to: 'transactions#index'
         get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/customer', to: 'customer#show'
+        get '/:id/merchant', to: 'merchant#show'
       end
       resources :invoices, only: [:index, :show]
     end
--- spec/requests/api/v1/invoices_relationship_spec.rb
+++ spec/requests/api/v1/invoices_relationship_spec.rb
@@ -3,20 +3,68 @@ require 'rails_helper'
 describe 'Invoice API relationship end point' do
   it 'returns transactions for a specific invoice' do
     invoice = create(:invoice)
+    invoice2 = create(:invoice)
+    create(:transaction, invoice_id: invoice.id)
+    create(:transaction, invoice_id: invoice.id)
+    create(:transaction, invoice_id: invoice2.id)
 
     get "/api/v1/invoices/#{invoice.id}/transactions"
     expect(response).to be_success
 
     transactions = JSON.parse(response.body)
-    expect(transactions).to eq(invoice.transactions)
+    expect(transactions.count).to eq(2)
   end
 
   it 'returns invoice items for a single invoice' do
     invoice = create(:invoice)
+    invoice2 = create(:invoice)
+    item = create(:item)
+    InvoiceItem.create!(invoice_id: invoice2.id, item_id: item.id, quantity: 2, unit_price: 234)
+    InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 1, unit_price: 1000)
+
     get "/api/v1/invoices/#{invoice.id}/invoice_items"
     expect(response).to be_success
 
     invoice_items = JSON.parse(response.body)
-    expect(invoice_items).to eq(invoice.invoice_items)
+    expect(invoice_items.count).to eq(1)
+  end
+
+  it 'returns items for a single invoice' do
+    create_list(:item, 2)
+    invoice = create(:invoice)
+    item = create(:item)
+    InvoiceItem.create!(invoice_id: invoice.id, item_id: item.id, quantity: 2, unit_price: 234)
+
+    get "/api/v1/invoices/#{invoice.id}/items"
+    expect(response).to be_success
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(1)
+  end
+
+  it 'returns customer for a specific invoice' do
+    customer = create(:customer)
+    create(:customer)
+    invoice = create(:invoice, customer_id: customer.id)
+
+    get "/api/v1/invoices/#{invoice.id}/customer"
+    expect(response).to be_success
+
+    customer = JSON.parse(response.body)
+
+    expect(customer['id']).to eq(invoice.customer.id)
+  end
+
+  it 'returns merchant for a specific invoice' do
+    merchant = create(:merchant)
+    create(:merchant)
+    invoice = create(:invoice, merchant_id: merchant.id)
+
+    get "/api/v1/invoices/#{invoice.id}/merchant"
+    expect(response).to be_success
+
+    merchant = JSON.parse(response.body)
+    expect(merchant['id']).to eq(invoice.merchant.id)
   end
 end
