f9b5e7f01c6eeb671c4b82209d52c079cab26b04
apoc64 <sschwedt@gmail.com>
2018-06-28 16:37:23 -0600

    moves most revenue merchant to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   default_scope { order(:id) }
+
+  def self.highest_revenue_items(quantity)
+    Item.select("items.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue").joins(:invoice_items).group(:id).order("revenue DESC").limit(quantity)
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+
+  def self.merchants_with_most_revenue(quantity)
+    Merchant.select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue").joins("INNER JOIN invoices ON invoices.merchant_id=merchants.id INNER JOIN invoice_items ON invoice_items.invoice_id=invoices.id").group("merchants.id").order("revenue DESC").limit(quantity)
+  end
 end
