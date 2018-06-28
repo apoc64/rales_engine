c75839d4fe31671f6122a4bc60fb565e961bfd96
apoc64 <sschwedt@gmail.com>
2018-06-27 18:34:57 -0600

    adds customer relationships

--- spec/requests/api/v1/customers_relations_spec.rb
+++ spec/requests/api/v1/customers_relations_spec.rb
@@ -12,4 +12,18 @@ describe 'Customer Relationships API' do
     invoices = JSON.parse(response.body)
     expect(invoices.count).to eq(3)
   end
+
+  it 'loads all transactions for that customer' do
+    create_list(:transaction, 3)
+    customer = create(:customer)
+    invoice = create(:invoice, customer_id: customer.id)
+    create(:transaction, invoice_id: invoice.id)
+    create(:transaction, invoice_id: invoice.id)
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+
+    expect(response).to be_successful
+    invoices = JSON.parse(response.body)
+    expect(invoices.count).to eq(2)
+  end
 end
