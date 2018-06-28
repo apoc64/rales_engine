d4ecac5c881971638457cba166a70fae493ceea4
apoc64 <sschwedt@gmail.com>
2018-06-28 16:26:28 -0600

    finds pending customers for merchant

--- app/controllers/api/v1/merchants/customers_controller.rb
+++ app/controllers/api/v1/merchants/customers_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::Merchants::CustomersController < ApplicationController
   def index
     # Will Revisit - Needs to pass spec
-    customers = Customer.find_by_sql("SELECT customers.* FROM customers INNER JOIN invoices ON invoices.customer_id=customers.id INNER JOIN transactions ON transactions.invoice_id=invoices.id WHERE invoices.merchant_id=#{params[:merchant_id]} AND (SELECT COUNT(result) FROM transactions WHERE result='success')=0 GROUP BY customers.id")
-    # EXCEPT SELECT customers.*, transactions.result, invoices.merchant_id FROM customers INNER JOIN invoices ON invoices.customer_id=customers.id INNER JOIN transactions ON transactions.invoice_id=invoices.id WHERE invoices.merchant_id=#{params[:merchant_id]} AND transactions.result='success'")
+    customers = Customer.find_by_sql("SELECT customers.* FROM customers
+      INNER JOIN invoices ON invoices.customer_id=customers.id
+      INNER JOIN transactions ON transactions.invoice_id=invoices.id
+      WHERE invoices.merchant_id=#{params[:merchant_id]}
+      EXCEPT SELECT customers.* FROM customers
+      INNER JOIN invoices ON invoices.customer_id=customers.id
+      INNER JOIN transactions ON transactions.invoice_id=invoices.id
+      WHERE transactions.result='success' AND invoices.merchant_id=#{params[:merchant_id]}")
+    # AND (SELECT COUNT(result) FROM transactions WHERE result='success')=0 GROUP BY customers.id")
+    # byebug
     # binding.pry
     render json: customers
   end
